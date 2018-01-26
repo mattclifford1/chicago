8d377d241a70b28873de457c4bcd28c28f55ba29
mattclifford1 <mattc863@hotmail.com>
2018-01-26 14:25:32 +0000

    trying to plot scatter

--- data_plot.py
+++ data_plot.py
@@ -1,7 +1,51 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
 
 data = np.load('dataClean.npy')
 
-plt.plot(data[0,:],data[1,:],'x')
+primary = ['ARSON', 'ASSAULT', 'BATTERY','BURGLARY','CONCEALED CARRY LICENSE VIOLATION','CRIM SEXUAL ASSAULT','CRIMINAL DAMAGE','CRIMINAL TRESPASS','DECEPTIVE PRACTICE','GAMBLING','HOMICIDE','HUMAN TRAFFICKING','INTERFERENCE WITH PUBLIC OFFICER','INTIMIDATION','KIDNAPPING','LIQUOR LAW VIOLATION','MOTOR VEHICLE THEFT','NARCOTICS','OBSCENITY','OFFENSE INVOLVING CHILDREN','OTHER NARCOTIC VIOLATION','OTHER OFFENSE','PROSTITUTION','PUBLIC INDECENCY','PUBLIC PEACE VIOLATION', 'ROBBERY','SEX OFFENSE','STALKING','THEFT','WEAPONS VIOLATION','NON-CRIMINAL']
+severity = [0.5,0.72,0.89,0.44,0.11,0.94,0.28,0.11,0.44,0.11,0.94,1.0,0.11,0.22,1.0,0.11,0.17,0.22,0.11,0.89,0.22,0.11,0.17,0.11,0.22,0.94,0.94,0.67,0.17,0.22,0]
+
+sev = np.zeros(data.shape[1])
+for i in range(data.shape[1]):
+	ind = primary.index(data[2,i])
+	sev[i] = severity[ind]
+
+
+
+X = data[0,:]
+Y = data[1,:]
+# create x-y points to be used in heatmap
+xi = np.linspace(X.min(),X.max(),1000)
+yi = np.linspace(Y.min(),Y.max(),1000)
+
+# Z is a matrix of x-y values
+zi = griddata((X, Y), sev, (xi[None,:], yi[:,None]), method='cubic')
+
+# I control the range of my colorbar by removing data 
+# outside of my range of interest
+zmin = 3
+zmax = 12
+zi[(zi<zmin) | (zi>zmax)] = None
+
+# Create the contour plot
+CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow,
+                  vmax=zmax, vmin=zmin)
+plt.colorbar()  
+plt.show()
+
+
+
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+x = data[0,:]
+y = data[1,:]
+
+# ax.scatter(x,y,sev, c='r', marker='o')
+ax.scatter(x, y, sev)
+# plt.savefig("demo.png")
+
 plt.show()
--- data_read.py
+++ data_read.py
@@ -3,20 +3,23 @@ import numpy as np
 from tqdm import tqdm
 
 #define column names
-colnames = ['ID', 'Case Number', 'Date', 'Block', 'IUCR','Primary Type','Description','Location Description','Arrest','Domestic','Beat','District','Ward','Community','FBI Code','XCoordinate','YCoordinate','Year','Updated On','Latitude','Longitude','Location']
+colnames = ['ID', 'Case Number', 'Date', 'Block', 'IUCR','PrimaryType','Description','Location Description','Arrest','Domestic','Beat','District','Ward','Community','FBI Code','XCoordinate','YCoordinate','Year','Updated On','Latitude','Longitude','Location']
 data = pandas.read_csv('crimes2016.csv', names=colnames)  #extract data
 
 #extract useful columns to lists
 X = data.XCoordinate.tolist()
 Y = data.YCoordinate.tolist()
+P = data.PrimaryType.tolist()
 
 #convert to numpy array for ease of use
 X = np.array(X)
 Y = np.array(Y)
+P = np.array(P)
 
 #get rid of first column containing string of column name
 X = np.delete(X,0)
 Y = np.delete(Y,0)
+P = np.delete(P,0)
 
 #get rid of any incomplete 'nan' entries from the data (using indexing for speed instead of deleting)
 dataLen = len(X)
@@ -26,15 +29,17 @@ for i in tqdm(range(len(X))):
 
 Xclean = np.zeros(dataLen)    #define arrays
 Yclean = np.zeros(dataLen)
+Pclean = [0] * dataLen
 ind = 0 
 for i in tqdm(range(len(X))):
 	if X[i] != 'nan':
 		Xclean[ind] = X[i]
 		Yclean[ind] = Y[i]
+		Pclean[ind] = P[i]
 		ind += 1
 
 #save array
-dataClean = np.array([Xclean,Yclean])
+dataClean = np.array([Xclean,Yclean,Pclean])
 np.save('dataClean.npy',dataClean)
 
 
