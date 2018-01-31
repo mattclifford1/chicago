70f0ae51fb86726e1b62cf4d76447576d4eae354
mattclifford1 <mattc863@hotmail.com>
2018-01-31 18:41:33 +0000

    GMM added

--- data_plot.py
+++ data_plot.py
@@ -1,44 +1,99 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+def main():
+	X, Y, sev = getData()
+	reduction = 1000
+	heatIm, x_min, y_min = makeHeatmap(X, Y, sev, reduction)
 
-dataCoord = np.load('dataCoord.npy')    #load coordinate data
-X = dataCoord[0,:]                      #and separate
-Y = dataCoord[1,:]
-sevStr = np.load('severity.npy')        #load severity
+	x1 = 1148220
+	y1 = 1899677
+	sevNorm = getSev(x1, y1, heatIm, reduction, x_min, y_min)
+	print('severity at (' + str(x1)+', ' + str(y1) +') is: ' + str(sevNorm))
+	heatData = heatmapData(heatIm)
 
-#lookup of numberal equivelant (should be done in a dict really)
-primary = ['ARSON', 'ASSAULT', 'BATTERY','BURGLARY','CONCEALED CARRY LICENSE VIOLATION','CRIM SEXUAL ASSAULT','CRIMINAL DAMAGE','CRIMINAL TRESPASS','DECEPTIVE PRACTICE','GAMBLING','HOMICIDE','HUMAN TRAFFICKING','INTERFERENCE WITH PUBLIC OFFICER','INTIMIDATION','KIDNAPPING','LIQUOR LAW VIOLATION','MOTOR VEHICLE THEFT','NARCOTICS','OBSCENITY','OFFENSE INVOLVING CHILDREN','OTHER NARCOTIC VIOLATION','OTHER OFFENSE','PROSTITUTION','PUBLIC INDECENCY','PUBLIC PEACE VIOLATION', 'ROBBERY','SEX OFFENSE','STALKING','THEFT','WEAPONS VIOLATION','NON-CRIMINAL']
-severity = [0.5,0.72,0.89,0.44,0.11,0.94,0.28,0.11,0.44,0.11,0.94,1.0,0.11,0.22,1.0,0.11,0.17,0.22,0.11,0.89,0.22,0.11,0.17,0.11,0.22,0.94,0.94,0.67,0.17,0.22,0]
+	from sklearn import mixture
+	# gmm = mixture.BayesianGaussianMixture(n_components=20).fit(heatData)
+	gmm = mixture.GaussianMixture(n_components=15).fit(heatData)
+	labels = gmm.predict(heatData)
+	print('done')
+	plt.scatter(heatData[:, 1], heatData[:, 0], c=labels, s=1, cmap='viridis')
+	plt.show()
+	# plot_results(heatData, gmm.predict(heatData), gmm.means_, gmm.covariances_, 0,'Gaussian Mixture')
 
-sev = np.zeros(len(sevStr))
-#assign severity str to numerical value
-for i in range(len(sevStr)):
-	ind = primary.index(sevStr[i])
-	sev[i] = severity[ind]
+def getData():
+	dataCoord = np.load('dataCoord.npy')    #load coordinate data
+	X = dataCoord[0,:]                      #and separate
+	Y = dataCoord[1,:]
+	sevStr = np.load('severity.npy')        #load severity
 
-#find min and 'width' for image space
-x_min = np.min(X)
-y_min = np.min(Y)
-x_len = int(np.max(X) - x_min)
-y_len = int(np.max(Y) - y_min)
+	#lookup of numberal equivelant (should be done in a dict really)
+	primary = ['ARSON', 'ASSAULT', 'BATTERY','BURGLARY','CONCEALED CARRY LICENSE VIOLATION','CRIM SEXUAL ASSAULT','CRIMINAL DAMAGE','CRIMINAL TRESPASS','DECEPTIVE PRACTICE','GAMBLING','HOMICIDE','HUMAN TRAFFICKING','INTERFERENCE WITH PUBLIC OFFICER','INTIMIDATION','KIDNAPPING','LIQUOR LAW VIOLATION','MOTOR VEHICLE THEFT','NARCOTICS','OBSCENITY','OFFENSE INVOLVING CHILDREN','OTHER NARCOTIC VIOLATION','OTHER OFFENSE','PROSTITUTION','PUBLIC INDECENCY','PUBLIC PEACE VIOLATION', 'ROBBERY','SEX OFFENSE','STALKING','THEFT','WEAPONS VIOLATION','NON-CRIMINAL']
+	severity = [0.5,0.72,0.89,0.44,0.11,0.94,0.28,0.11,0.44,0.11,0.94,1.0,0.11,0.22,1.0,0.11,0.17,0.22,0.11,0.89,0.22,0.11,0.17,0.11,0.22,0.94,0.94,0.67,0.17,0.22,0]
 
-#shift to [0,0] to save figure space
-x_shift = X - x_min
-y_shift = Y - y_min
+	sev = np.zeros(len(sevStr))
+	#assign severity str to numerical value
+	for i in range(len(sevStr)):
+		ind = primary.index(sevStr[i])
+		sev[i] = severity[ind]
+	return X, Y, sev
 
-data_reduce = 100   #make image a sensible size
+def makeHeatmap(X, Y, sev, data_reduce):
+	#reduce data size
+	X = X/data_reduce
+	Y = Y/data_reduce
 
-#define image size
-heatIm =  np.zeros([int(np.ceil((y_len+1)/data_reduce)), int(np.ceil((x_len+1)/data_reduce))], dtype=np.uint16)
+	#find min and 'width' for image space
+	x_min = np.min(X)
+	y_min = np.min(Y)
+	x_len = np.max(X) - x_min
+	y_len = np.max(Y) - y_min
 
-#sum severities for image locations, n.b. x axis has to be flipped to match image coordinates
-for i in range(len(X)):
-	heatIm[abs(int(y_shift[i]/data_reduce)-heatIm.shape[0]+1), int(x_shift[i]/data_reduce)] += int(sev[i]*10)
+	#shift to [0,0] to save figure space
+	x_shift = X - x_min
+	y_shift = Y - y_min
 
-#plot and save image
-plt.imshow(heatIm,cmap='hot')
-plt.xticks([])
-plt.yticks([])
-plt.savefig('heatmap.png')
-print('saved')
+	#define image size
+	heatIm =  np.zeros([int(np.ceil((y_len+1))), int(np.ceil((x_len+1)))], dtype=np.uint16)
+
+	#sum severities for image locations, n.b. x axis has to be flipped to match image coordinates
+	for i in range(len(X)):
+		heatIm[abs(int(y_shift[i])-heatIm.shape[0]+1), int(x_shift[i])] += int(sev[i]*10)
+
+	#plot and save image
+	plt.imshow(heatIm,cmap='hot')
+	plt.xticks([])
+	plt.yticks([])
+	plt.savefig('heatmap.png')
+	print('saved')
+	# plt.clf()
+	return heatIm, x_min, y_min
+
+def getSev(x1, y1, heatIm, data_reduce, x_min, y_min):  #x and y are point from original scaling from chicago data set
+	x1 = x1/data_reduce
+	y1 = y1/data_reduce
+
+	x1_shift = int(np.round(x1 - x_min))
+	y1_shift = int(np.round(y1 - y_min))
+
+	sev1 = heatIm[y1_shift,x1_shift]
+	maxSev = np.max(heatIm)
+	sevNorm = sev1/maxSev
+
+	return sevNorm
+
+def heatmapData(heatIm):    #converts pixels to crime data points of the heatmap
+	data_points = sum(sum(heatIm))   #number of crime units
+	heatData = np.zeros([data_points,2])
+	count = 0
+	for i in range(heatIm.shape[0]):
+		for j in range(heatIm.shape[1]):
+			units = heatIm[i,j]
+			for k in range(units):
+				heatData[count,0] = i
+				heatData[count,1] = j
+				count += 1
+	return heatData
+
+if __name__ == "__main__":
+ 	main()
