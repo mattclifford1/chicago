1ae7882d08465bd5ddecf2b6356141c24bfa5870
mattclifford1 <mattc863@hotmail.com>
2018-02-12 11:16:06 +0000

    asd

--- data_plot.py
+++ data_plot.py
@@ -67,8 +67,13 @@ def getData():
 	sevStr = np.load('severity.npy')        #load severity
 
 	#lookup of numberal equivelant (should be done in a dict really)
+	# import pandas
+	# colnames = 
+	# data = pandas.read_csv('crimes2016.csv', names=colnames)  #extract data
+	# IUCR = data.IUCR_Codes.tolist()
 	primary = ['ARSON', 'ASSAULT', 'BATTERY','BURGLARY','CONCEALED CARRY LICENSE VIOLATION','CRIM SEXUAL ASSAULT','CRIMINAL DAMAGE','CRIMINAL TRESPASS','DECEPTIVE PRACTICE','GAMBLING','HOMICIDE','HUMAN TRAFFICKING','INTERFERENCE WITH PUBLIC OFFICER','INTIMIDATION','KIDNAPPING','LIQUOR LAW VIOLATION','MOTOR VEHICLE THEFT','NARCOTICS','OBSCENITY','OFFENSE INVOLVING CHILDREN','OTHER NARCOTIC VIOLATION','OTHER OFFENSE','PROSTITUTION','PUBLIC INDECENCY','PUBLIC PEACE VIOLATION', 'ROBBERY','SEX OFFENSE','STALKING','THEFT','WEAPONS VIOLATION','NON-CRIMINAL']
 	severity = [0.5,0.72,0.89,0.44,0.11,0.94,0.28,0.11,0.44,0.11,0.94,1.0,0.11,0.22,1.0,0.11,0.17,0.22,0.11,0.89,0.22,0.11,0.17,0.11,0.22,0.94,0.94,0.67,0.17,0.22,0]
+
 
 	sev = np.zeros(len(sevStr))
 	#assign severity str to numerical value
