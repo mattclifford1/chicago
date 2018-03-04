808284b6b7ca11dfb0919b8d6db78578a3b006fb
mattclifford1 <mattc863@hotmail.com>
2018-03-04 14:41:17 +0000

    fit

--- icplot.py
+++ icplot.py
@@ -6,8 +6,12 @@ m0 = np.load('modelsFull1.npy')
 m1 = np.load('modelsFull50200.npy')
 m2 = np.load('modelsFull160.npy')
 sevData = np.load('sevData.npy')
-
+m = np.load('snowy2.npy',encoding='bytes')
+print(m.shape)
 models = [0]*(8+6)
+models = [0]*(11)
+# for i in range(11):
+
 # models[0] = m1[0]
 # models[1] = m1[1]
 # models[2] = m1[2]
@@ -15,21 +19,21 @@ models = [0]*(8+6)
 # models[4] = m2[0]
 # models[5] = m1[4] 
 
-models[0] = m0[0]
-models[1] = m0[1]
-models[2] = m0[2]
-models[3] = m0[3]
-models[4] = m0[4]
-models[5] = m0[5]
-models[6] = m0[6]
-models[7] = m0[7]
-models[8] = m1[0]
-models[9] = m1[1]
-models[10] = m1[2]
-models[11] = m1[3]
-models[12] = m2[0]
-models[13] = m1[4]
-
+# models[0] = m0[0]
+# models[1] = m0[1]
+# models[2] = m0[2]
+# models[3] = m0[3]
+# models[4] = m0[4]
+# models[5] = m0[5]
+# models[6] = m0[6]
+# models[7] = m0[7]
+# models[8] = m1[0]
+# models[9] = m1[1]
+# models[10] = m1[2]
+# models[11] = m1[3]
+# models[12] = m2[0]
+# models[13] = m1[4]
+models = m
 n_components = [50,70,100,130,160,200]
 n_components = [5,10,14,18,23,30,40,50]
 n_components = [5,10,14,18,23,30,40,50,51,70,100,130,160,200]
